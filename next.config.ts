489a8b3dd02a8a660666b76316d26d623236ae46
Ahmed Midani <mideni.ahmed@gmail.com>
2024-11-03 21:06:35 +0100

    chapter 10/16

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,9 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  // experimental: {
+  //   ppr: 'incremental', // The 'incremental' value allows you to adopt PPR for specific routes.
+  // },
 };
 
 export default nextConfig;
